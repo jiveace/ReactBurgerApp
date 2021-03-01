33c05dd1abfb4f74da8c9e02912b16bc71ad5700
Iain A Maxwell <straightedgescotsman@gmail.com>
2021-03-01 14:56:05 +0000

    Adding auth data to store

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -8,10 +8,11 @@ export const authStart = () => {
     };
 };
 
-export const authSuccess = authData => {
+export const authSuccess = (token, userId) => {
     return {
         type: actionTypes.AUTH_SUCCESS,
-        authData
+        idToken: token,
+        userId
     };
 };
 
@@ -37,11 +38,11 @@ export const auth = (email, password, isSignUp) => {
         axios.post(url, authData)
             .then(response => {
                 console.log(response);
-                dispatch(authSuccess(response.data))
+                dispatch(authSuccess(response.data.idToken, response.data.localId))
             })
             .catch(err => {
                 console.log(err);
-                dispatch(authFail(err))
+                dispatch(authFail(err.response.data.error))
             });
     };
 };
