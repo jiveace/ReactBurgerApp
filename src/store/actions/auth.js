16a802fc1f0310619862bb8360a4f21c86459208
Iain A Maxwell <straightedgescotsman@gmail.com>
2021-03-18 13:32:45 +0000

    Moving aauth timeout check to saga

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -23,19 +23,21 @@ export const authFail = error => {
 };
 
 export const logout = () => {
-    // localStorage.removeItem('token');
-    // localStorage.removeItem('expirationDate');
-    // localStorage.removeItem('userId');
     return {
         type: actionTypes.AUTH_INITIATE_LOGOUT
     };
 };
 
+export const logoutSucceed = () => {
+    return {
+        type: actionTypes.AUTH_LOGOUT
+    };
+};
+
 export const checkAuthTimeout = expirationTime => {
-    return dispatch => {
-        setTimeout(() => {
-            dispatch(logout());
-        }, expirationTime * 1000)
+    return {
+        type: actionTypes.AUTH_CHECK_TIMEOUT,
+        expirationTime
     };
 };
 
@@ -47,7 +49,7 @@ export const authCheckState = () => {
         } else {
             const expirationDate = new Date(localStorage.getItem('expirationDate'));
             if (expirationDate <= new Date()) {
-                dispatch(logout())
+                dispatch(logout());
             } else {
                 const userId = localStorage.getItem('userId');
                 dispatch(authSuccess(token, userId));
@@ -67,7 +69,7 @@ export const auth = (email, password, isSignUp) => {
         };
         let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAKY-Ae8-zoYRD_qgn3eseChmx_UAY18bg';
         if (!isSignUp) {
-            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAKY-Ae8-zoYRD_qgn3eseChmx_UAY18bg'
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAKY-Ae8-zoYRD_qgn3eseChmx_UAY18bg';
         }
         axios.post(url, authData)
             .then(response => {
@@ -75,12 +77,12 @@ export const auth = (email, password, isSignUp) => {
                 localStorage.setItem('token', response.data.idToken);
                 localStorage.setItem('expirationDate', expirationDate);
                 localStorage.setItem('userId', response.data.localId);
-                dispatch(authSuccess(response.data.idToken, response.data.localId))
-                dispatch(checkAuthTimeout(response.data.expiresIn))
+                dispatch(authSuccess(response.data.idToken, response.data.localId));
+                dispatch(checkAuthTimeout(response.data.expiresIn));
             })
             .catch(err => {
                 console.log(err);
-                dispatch(authFail(err.response.data.error))
+                dispatch(authFail(err.response.data.error));
             });
     };
 };
